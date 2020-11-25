9c68594247ddfaf28ae0ac5bc68b9f26072e2f8b
Jose Miralles <josemiralles1991@gmail.com>
2020-11-25 16:02:28 -0500

    Switching roles

--- url_shortener/URLShortener/db/migrate/20201125202944_create_shrotened_urls.rb
+++ url_shortener/URLShortener/db/migrate/20201125202944_create_shrotened_urls.rb
@@ -1,9 +1,15 @@
 class CreateShrotenedUrls < ActiveRecord::Migration[5.2]
   def change
+
     create_table :shrotened_urls do |t|
-      t.string :long_url, null: true
+      t.string :long_url, null: false
       t.string :short_url, null: true
-      
+      t.integer :user_id, null: false
+      t.timestamps
     end
+
+    add_index :shortened_url, :short_url, unique: true
+    add_index :shortened_url, :user_id, unique: true
+
   end
 end
